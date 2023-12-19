1ef55dc933fb1c5999489abd74a8a6ad79981806
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-12-20 01:04:59 +0530

    add changes in card

--- imdb/src/App.js
+++ imdb/src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './Components/Header/Header';
 import Home from './Pages/Home/Home';
+import MovieList from './Components/movieList/movieList';
+import Movie from './Pages/movieDetails/movie';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header/>
-        <Routes>
-          <Route index element={<Home/>}/>
-          <Route path='movie/:id' element={<h1>Movie Details Page</h1>}/>
-          <Route path='movies/:type' element={<h1>Movie List Page</h1>}/>
-          <Route path='/*' element={<h1>Error Page</h1>}/>
-
-        </Routes>
-      </Router>
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<Movie />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
   );
 }

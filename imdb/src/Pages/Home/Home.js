b6063d52995631ce8df876d3540bdcaa9efa891e
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-12-19 15:28:45 +0530

    add css to home page

--- imdb/src/Pages/Home/Home.js
+++ imdb/src/Pages/Home/Home.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react"
-import "./home.css"
+import "./Home.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from "react-router-dom";
-import MovieList from "../../components/movieList/movieList";
+import MovieList from "../../Components/movieList/movieList";
 
 const Home = () => {
 
